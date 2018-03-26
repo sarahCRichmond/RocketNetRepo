a6f8c850a7ae8944fe2421f47e3f3c4ba51b968c
Michael Diep <michaeldiep97@gmail.com>
2018-03-25 22:38:15 -0500

    More graphical tweaks, Added ItemController class and corresponding cshtml files to display items. TODO: Pokemon creation etc under the PokemonController should be limited to admin useage.

---
+++ RocketNet/Controllers/ItemsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RocketNet.Models;
+
+namespace RocketNet.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private PokeDBContext db = new PokeDBContext();
+
+        // GET: Items
+        public ActionResult Index()
+        {
+            return View(db.Pokes.ToList());
+        }
+
+        // GET: Items/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pokemon pokemon = db.Pokes.Find(id);
+            if (pokemon == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pokemon);
+        }
+
+        public ActionResult List()
+        {
+            return View(db.Pokes.ToList());
+        }
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
